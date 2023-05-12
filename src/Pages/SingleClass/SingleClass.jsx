f55a385e8b110c9b0d23d774f65f045a1921173f
rabiulahsan <ra.rabiulahsan@gmail.com>
2023-05-13 02:58:40 +0600

    single class details added

--- src/Pages/SingleClass/SingleClass.jsx
+++ src/Pages/SingleClass/SingleClass.jsx
@@ -7,6 +7,7 @@ import RightSideNavSingleClass from "./RightSideNavSingleClass";
 const SingleClass = () => {
   const classData = useLoaderData();
   const id = useParams().id;
+
   return (
     <div className="flex w-full">
       <div className="w-1/5 ">
@@ -14,7 +15,7 @@ const SingleClass = () => {
       </div>
       <div className="w-3/5 bg-[#ebeaf8]">
         <div className="">
-          <SingleClassDetails></SingleClassDetails>
+          <SingleClassDetails classData={classData}></SingleClassDetails>
         </div>
         <div className=""></div>
       </div>
@@ -26,3 +27,50 @@ const SingleClass = () => {
 };
 
 export default SingleClass;
+
+// const [stds, setStds] = useState([]);
+// const [stdArr, setStdArr] = useState([]);
+// const [submitted, setSubmitted] = useState(false);
+
+// useEffect(() => {
+//   fetch("/students.json")
+//     .then((res) => res.json())
+//     .then((data) => setStds(data));
+// }, []);
+
+// const handleSubmitAttendance = () => {
+//   console.log(stdArr);
+//   setSubmitted(true);
+// };
+// return (
+//   <div>
+//     <p className="font-bold"> Hello World</p>
+//     <p>{stds.length}</p>
+
+//     <table className="table-fixed w-3/5 border border-gray-500">
+//       {/* head */}
+//       <thead>
+//         <tr>
+//           <th className="py-5">ID</th>
+//           <th>Name</th>
+//           <th>Attendance</th>
+//           <th>P/A</th>
+//         </tr>
+//       </thead>
+//       <tbody>
+//         {stds.map((std) => (
+//           <StdRow key={std.std_id} std={std} setStdArr={setStdArr}></StdRow>
+//         ))}
+//       </tbody>
+//     </table>
+//     {submitted ? (
+//       <Button disabled onClick={handleSubmitAttendance} variant="contained">
+//         Submit
+//       </Button>
+//     ) : (
+//       <Button onClick={handleSubmitAttendance} variant="contained">
+//         Submit
+//       </Button>
+//     )}
+//   </div>
+// );
--- src/Pages/SingleClass/SingleClassDetails.jsx
+++ src/Pages/SingleClass/SingleClassDetails.jsx
@@ -1,7 +1,35 @@
 import React from "react";
+import dayjs from "dayjs";
 
-const SingleClassDetails = () => {
-  return <div>Details</div>;
+const SingleClassDetails = ({ classData }) => {
+  const { batch, semester, subjCode, totalStudents, subject } = classData;
+  const dateValue = dayjs();
+  const formattedDate = dateValue.format("dddd, MMMM D, YYYY");
+  return (
+    <div className="my-[3%] mx-[3%] p-[5%] bg-white rounded-[10px] flex justify-between items-center">
+      <div className="text-blue-900">
+        <p className="">
+          <span className="font-semibold">Course:</span>
+          {"    " + subject}
+        </p>
+        <p className="my-3">
+          <span className="font-semibold">Code:</span>
+          {"    " + subjCode}
+        </p>
+        <p className="">
+          <span className="font-semibold">Semester:</span>
+          {" " + semester}
+        </p>
+        <p className="my-3">
+          <span className="font-semibold">Total Students:</span>
+          {"    " + totalStudents}
+        </p>
+      </div>
+      <div className="text-blue-900 font-semibold">
+        <p>{"    " + formattedDate}</p>
+      </div>
+    </div>
+  );
 };
 
 export default SingleClassDetails;
