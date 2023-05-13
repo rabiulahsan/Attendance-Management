061089f46c7c732fcb9f18a39906d7b4052b155c
rabiulahsan <ra.rabiulahsan@gmail.com>
2023-05-13 20:26:35 +0600

    google sign in added

---
+++ src/Pages/Shared/GoogleSignIn/GoogleSignIn.jsx
@@ -0,0 +1,36 @@
+import React, { useContext } from "react";
+import { FcGoogle } from "react-icons/fc";
+import { AuthContext } from "../../../Provider/AuthProvider";
+import { useNavigate } from "react-router-dom";
+
+const GoogleSignIn = () => {
+  const { googleLogin } = useContext(AuthContext);
+
+  const navigate = useNavigate();
+  const handleGoogleSignIn = () => {
+    googleLogin()
+      .then((result) => {
+        const user = result.user;
+        console.log(user);
+        navigate("/home");
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+  return (
+    <div
+      className=" border border-blue-500 flex text-center text-white cursor-pointer font-semibold"
+      onClick={handleGoogleSignIn}
+    >
+      <span className="text-2xl p-2">
+        <FcGoogle></FcGoogle>
+      </span>
+      <span className="bg-blue-500 w-full p-2 hover:bg-blue-600">
+        Sign in With Google
+      </span>
+    </div>
+  );
+};
+
+export default GoogleSignIn;
--- src/Pages/Signup/Signup.jsx
+++ src/Pages/Signup/Signup.jsx
@@ -2,6 +2,7 @@ import React, { useContext } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { AuthContext } from "../../Provider/AuthProvider";
 import { updateProfile } from "firebase/auth";
+import GoogleSignIn from "../Shared/GoogleSignIn/GoogleSignIn";
 
 const Signup = () => {
   const { createUser, logOut } = useContext(AuthContext);
@@ -42,52 +43,64 @@ const Signup = () => {
 
   return (
     <div className="flex justify-center items-center bg-blue-300 h-[710px]">
-      <form
-        onSubmit={handleSignUp}
-        className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 w-1/5 "
-      >
-        <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2">
-            Username
-          </label>
-          <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            type="text"
-            name="name"
-            placeholder="Username"
-          ></input>
-        </div>
-        <div className="mb-6">
-          <label className="block text-gray-700 text-sm font-bold mb-2">
-            E-mail
-          </label>
-          <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            type="email"
-            name="email"
-            placeholder="E-mail"
-          ></input>
-        </div>
-        <div className="mb-6">
-          <label className="block text-gray-700 text-sm font-bold mb-2">
-            Password
-          </label>
-          <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-            type="password"
-            name="password"
-            placeholder="Password"
-          ></input>
+      <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 w-1/5 ">
+        <form onSubmit={handleSignUp} className="">
+          <div className="mb-4">
+            <label className="block text-gray-700 text-sm font-bold mb-2">
+              Username
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              type="text"
+              name="name"
+              placeholder="Username"
+            ></input>
+          </div>
+          <div className="mb-6">
+            <label className="block text-gray-700 text-sm font-bold mb-2">
+              E-mail
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              type="email"
+              name="email"
+              placeholder="E-mail"
+            ></input>
+          </div>
+          <div className="mb-6">
+            <label className="block text-gray-700 text-sm font-bold mb-2">
+              Password
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+              type="password"
+              name="password"
+              placeholder="Password"
+            ></input>
+          </div>
+
+          <div className="cursor-pointer text-center bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+            <input
+              className="btn btn-primary cursor-pointer"
+              type="submit"
+              value="Signup"
+            />
+          </div>
+        </form>
+        <p className="text-center text-gray-400 my-5">or</p>
+        <div className="my-5 mb-10">
+          <GoogleSignIn></GoogleSignIn>
         </div>
 
-        <div className="cursor-pointer text-center bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
-          <input
-            className="btn btn-primary cursor-pointer"
-            type="submit"
-            value="Signup"
-          />
-        </div>
-      </form>
+        <p className="flex flex-col text-center text-gray-500">
+          Already have an account?
+          <Link to={"/"}>
+            <span className="text-blue-800 font-semibold hover:underline">
+              Login
+            </span>
+          </Link>
+        </p>
+      </div>
     </div>
   );
 };
